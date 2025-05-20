56bd3c1544eea0d86f31b2c8ff8e5f24454e9727
harshitsaraswat <165893581+harshitsaraswat@users.noreply.github.com>
2025-05-20 11:41:21 +0530

    Add files via upload

---
+++ assignment3.js
@@ -0,0 +1,9 @@
+function bw() {
+    // window.alert("Color Change");
+    document.getElementById("i1").src="../download.jpeg";
+}
+
+function clr() {
+    // alert();
+    document.getElementById("i1").src="../download.jpg";
+}
